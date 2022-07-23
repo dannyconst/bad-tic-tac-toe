89d264423cb246802d6f271ce6594e5e2b81cc51
dannyconst <109353289+dannyconst@users.noreply.github.com>
2022-07-23 20:12:27 +0600

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -4,44 +4,68 @@
 class Square {
     private:
         sf::Sprite sprite;
-        sf::Texture texture;
+        sf::Texture* texture;
 
     public:
         Square(float x, float y) {
-            if (!texture.loadFromFile("assets/x.png")) {
+            texture = new sf::Texture;
+            if (!texture->loadFromFile("assets/x.png")) {
                 std::cout << "Texture failed to load\n";
             }
 
             sprite.setPosition(sf::Vector2f(x, y));
             sprite.setScale(sf::Vector2f(6.25f, 6.25f));
-            sprite.setTexture(texture);
-            sprite.setColor(sf::Color::Red);
+            sprite.setTexture(*texture);
         }
 
         void render(sf::RenderTarget* window) {
             window->draw(sprite);
+        }
+
+        void addTransparency() {
+            sprite.setColor(sf::Color(255, 255, 255, 128));
+        }
+
+        void removeTransparency() {
+            sprite.setColor(sf::Color(255, 255, 255, 255));
+        }
+
+        void cleanUp() {
+            delete texture;
         }
 };
 
 class Circle {
     private:
         sf::Sprite sprite;
-        sf::Texture texture;
+        sf::Texture* texture;
 
     public:
         Circle(float x, float y) {
-            if (!texture.loadFromFile("assets/o.png")) {
+            texture = new sf::Texture;
+            if (!texture->loadFromFile("assets/o.png")) {
                 std::cout << "Texture failed to load\n";
             }
 
             sprite.setPosition(sf::Vector2f(x, y));
             sprite.setScale(sf::Vector2f(6.25f, 6.25f));
-            sprite.setTexture(texture);
-            sprite.setColor(sf::Color::Blue);
+            sprite.setTexture(*texture);
         }
 
         void render(sf::RenderTarget* window) {
             window->draw(sprite);
+        }
+
+        void addTransparency() {
+            sprite.setColor(sf::Color(255, 255, 255, 128));
+        }
+
+        void removeTransparency() {
+            sprite.setColor(sf::Color(255, 255, 255, 255));
+        }
+
+        void cleanUp() {
+            delete texture;
         }
 };
 
@@ -49,6 +73,8 @@ class Circle {
 bool gameOver = false;
 bool endGameOverLoop = false;
 bool playersTurn = false;
+bool playerLost = false;
+bool playerWon = false;
 
 // Score
 int playerScore = 0;
@@ -58,7 +84,18 @@ int opponentScore = 0;
 std::vector <Square> square;
 std::vector <Circle> circle;
 
+// Grid
+sf::RectangleShape lines[4];
 sf::RectangleShape boxes[9];
+
+// Font
+sf::Font font;
+
+// Text
+sf::Text playerScoreText;
+sf::Text opponentScoreText;
+sf::Text winStatusText;
+sf::Text restartText;
 
 // Matrix
 // 0 = empty
@@ -77,9 +114,21 @@ int delay = 0;
 bool startDelay = false;
 
 // Functions
-void restart() {
-    std::cout << "\nRestart\n";
+void cleanUp() {
+    int squareSize = square.size();
+    for (int i = 0; i < squareSize; i++) {
+        square.at(i).cleanUp();
+    }
 
+    int circleSize = circle.size();
+    for (int i = 0; i < circleSize; i++) {
+        circle.at(i).cleanUp();
+    }
+}
+
+void restart() {
+    cleanUp();
+    
     for (int i = 0; i < 3; i++) {
         for (int j = 0; j < 3; j++) {
             matrix[i][j] = 0;
@@ -87,26 +136,31 @@ void restart() {
     }
 
     int squareSize = square.size();
-
     for (int i = 0; i < squareSize; i++) {
         square.erase(square.begin() + 1);
     }
 
     int circleSize = circle.size();
-
     for (int i = 0; i < circleSize; i++) {
         circle.erase(circle.begin() + 1);
     }
 
-    if (playersTurn) {
-        std::cout << "Player's turn\n";
+    // Disable transparency
+    for (int i = 0; i < square.size(); i++) {
+        square.at(i).removeTransparency();
     }
-    else {
-        std::cout << "Opponent's turn\n";
+    for (int i = 0; i < circle.size(); i++) {
+        circle.at(i).removeTransparency();
+    }
+    for (int i = 0; i < 4; i++) {
+        lines[i].setFillColor(sf::Color::Black);
     }
 
     delay = 0;
     startDelay = false;
+
+    playerLost = false;
+    playerWon = false;
 
     gameOver = false;
     endGameOverLoop = false;
@@ -392,20 +446,29 @@ void AI() {
     }
     else {
         chooseRand();
-        std::cout << "Random\n";
     }
 }
 
 void win() {
-    std::cout << "\nPlayer wins!\n";
-    playerScore++;
-    playersTurn = false;
+    if (!playerWon) {
+        playerScore++;
+        playersTurn = false;
+        playerWon = true;
+        gameOver = true;
+        winStatusText.setString("Player wins");
+        winStatusText.setCharacterSize(250);
+    }
 }
 
 void lose() {
-    std::cout << "\nOpponent wins!\n";
-    opponentScore++;
-    playersTurn = true;
+    if (!playerLost) {
+        opponentScore++;
+        playersTurn = true;
+        playerLost = true;
+        gameOver = true;
+        winStatusText.setString("Opponent wins");
+        winStatusText.setCharacterSize(200);
+    }
 }
 
 void randTurn() {
@@ -445,9 +508,12 @@ void game() {
                 delay = 0;
             }
             else if (marked >= 9) {
-                std::cout << "\nDraw\n";
                 randTurn();
                 gameOver = true;
+                winStatusText.setString("Draw");
+                winStatusText.setCharacterSize(250);
+                sf::FloatRect bounds = winStatusText.getLocalBounds();
+                winStatusText.setPosition(sf::Vector2f(300.f - bounds.width / 2.f, bounds.height));
             }
         }
         else if (delay < 30 && startDelay) {
@@ -459,11 +525,9 @@ void game() {
     for (int i = 0; i < 3; i++) {
         if (matrix[i][0] == 1 && matrix[i][1] == 1 && matrix[i][2] == 1) {
             win();
-            gameOver = true;
         }
         else if (matrix[i][0] == 2 && matrix[i][1] == 2 && matrix[i][2] == 2) {
             lose();
-            gameOver = true;
         }
     }
 
@@ -471,33 +535,98 @@ void game() {
     for (int i = 0; i < 3; i++) {
         if (matrix[0][i] == 1 && matrix[1][i] == 1 && matrix[2][i] == 1) {
             win();
-            gameOver = true;
         }
         else if (matrix[0][i] == 2 && matrix[1][i] == 2 && matrix[2][i] == 2) {
             lose();
-            gameOver = true;
         }
     }
 
     // Diagonal 1
     if (matrix[0][2] == 1 && matrix[1][1] == 1 && matrix[2][0] == 1) {
         win();
-        gameOver = true;
     }
     else if (matrix[0][2] == 2 && matrix[1][1] == 2 && matrix[2][0] == 2) {
         lose();
-        gameOver = true;
     }
 
     // Diagonal 2
     if (matrix[0][0] == 1 && matrix[1][1] == 1 && matrix[2][2] == 1) {
         win();
-        gameOver = true;
     }
     else if (matrix[0][0] == 2 && matrix[1][1] == 2 && matrix[2][2] == 2) {
         lose();
-        gameOver = true;
     }
+}
+
+void initText() {
+    if (!font.loadFromFile("assets/game_over.ttf")) {
+        std::cout << "Failed to load font\n";
+    }
+
+    // Player score
+    playerScoreText.setFont(font);
+    playerScoreText.setPosition(sf::Vector2f(20.f, 0.f));
+    playerScoreText.setString("Players score:");
+    playerScoreText.setCharacterSize(100);
+    playerScoreText.setFillColor(sf::Color::Black);
+
+    
+    // Opponent score
+    opponentScoreText.setFont(font);
+    opponentScoreText.setPosition(sf::Vector2f(20.f, 60.f));
+    opponentScoreText.setString("Opponents score:");
+    opponentScoreText.setCharacterSize(100);
+    opponentScoreText.setFillColor(sf::Color::Black);
+
+    // Win status
+    winStatusText.setFont(font);
+    winStatusText.setPosition(sf::Vector2f(20.f, 100.f));
+    winStatusText.setString("Player wins");
+    winStatusText.setCharacterSize(230);
+    winStatusText.setFillColor(sf::Color::Black);
+    winStatusText.setStyle(sf::Text::Bold);
+
+    // Restart text
+    restartText.setFont(font);
+    restartText.setPosition(sf::Vector2f(100.f, 400.f));
+    restartText.setString("Press space to continue");
+    restartText.setCharacterSize(100);
+    restartText.setFillColor(sf::Color::Black);
+}
+
+void initGrid() {
+    // Init lines
+    for (int i = 0; i < 2; i++) {
+        lines[i] = sf::RectangleShape(sf::Vector2f(600.f, 8.f));
+        lines[i].setFillColor(sf::Color::Black);
+    }
+    for (int i = 2; i < 4; i++) {
+        lines[i] = sf::RectangleShape(sf::Vector2f(8.f, 600.f));
+        lines[i].setFillColor(sf::Color::Black);
+    }
+
+    // Set position
+    lines[0].setPosition(sf::Vector2f(0.f, 200.f - 4.f));
+    lines[1].setPosition(sf::Vector2f(0.f, 400.f - 4.f));
+    lines[2].setPosition(sf::Vector2f(200.f - 4.f, 0.f));
+    lines[3].setPosition(sf::Vector2f(400.f - 4.f, 0.f));
+}
+
+void gameOverMenu() {
+    for (int i = 0; i < square.size(); i++) {
+        square.at(i).addTransparency();
+    }
+
+    for (int i = 0; i < circle.size(); i++) {
+        circle.at(i).addTransparency();
+    }
+
+    for (int i = 0; i < 4; i++) {
+        lines[i].setFillColor(sf::Color(128, 128, 128, 255));
+    }
+
+    playerScoreText.setString("Players score: " + std::to_string(playerScore));
+    opponentScoreText.setString("Opponents score: " + std::to_string(opponentScore));
 }
 
 int main() {
@@ -517,18 +646,16 @@ int main() {
         (videoMode.getDesktopMode().height / 2) - videoMode.height / 2)
     );
 
+    initText();
+
     window.setFramerateLimit(20);
 
     if (rand() % 2 == 0) {
         playersTurn = true;
-        std::cout << "Player starts\n";
     }
     else {
         playersTurn = false;
-        std::cout << "Opponent starts\n";
     }
-
-    sf::RectangleShape lines[4];
 
     // Init Boxes
     for (int i = 0; i < 3; i++) {
@@ -544,21 +671,7 @@ int main() {
         boxes[i].setSize(sf::Vector2f(200.f, 200.f));
     }
 
-    // Init lines
-    for (int i = 0; i < 2; i++) {
-        lines[i] = sf::RectangleShape(sf::Vector2f(600.f, 8.f));
-        lines[i].setFillColor(sf::Color::Black);
-    }
-    for (int i = 2; i < 4; i++) {
-        lines[i] = sf::RectangleShape(sf::Vector2f(8.f, 600.f));
-        lines[i].setFillColor(sf::Color::Black);
-    }
-
-    // Set position
-    lines[0].setPosition(sf::Vector2f(0.f, 200.f - 4.f));
-    lines[1].setPosition(sf::Vector2f(0.f, 400.f - 4.f));
-    lines[2].setPosition(sf::Vector2f(200.f - 4.f, 0.f));
-    lines[3].setPosition(sf::Vector2f(400.f - 4.f, 0.f));
+    initGrid();
 
     // Game loop
     while (window.isOpen()) {
@@ -577,9 +690,7 @@ int main() {
             game();
         }
         else if (gameOver && !endGameOverLoop) {
-            std::cout << "Player score: " << playerScore << "\n";
-            std::cout << "Opponent's score: " << opponentScore << "\n";
-            std::cout << "\nPress 'Space' to continue\n";
+            gameOverMenu();
             endGameOverLoop = true;
         }
 
@@ -601,9 +712,19 @@ int main() {
             window.draw(lines[i]);
         }
 
+        // Text
+        if (gameOver) {
+            window.draw(playerScoreText);
+            window.draw(opponentScoreText);
+            window.draw(winStatusText);
+            window.draw(restartText);
+        }
+
         // Display
         window.display();
     }
+
+    cleanUp();
 
     return 0;
 }
